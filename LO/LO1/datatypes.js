232d4b5d8bc6805ac909dcf66f73669e3f0ffab0
aila marie fruponga <ailamariefruponga@gmail.com>
2021-02-08 18:53:44 +0800

    UC2__TESDA
    assignment

---
+++ LO/LO1/datatypes.js
@@ -0,0 +1,32 @@
+//Primitive Type
+
+const name = 'Jireh lim'; //string
+console.log(name);
+
+const age = 25; //integer
+console.log(age);
+
+const hungry = true; //boolean
+console.log(hungry);
+
+const money = null; //null
+console.log(money);
+
+let dogname; //undefined
+console.log(dogname);
+
+const favFood = ['adobo, sinigang, lumpia ']; //reference type - object 
+console.log(favFood);
+
+//Object literal
+const address = {
+    city: 'caloocan City',
+    barangay: 'libis',
+    street: 'Reparo'
+}
+console.log(address);
+
+const time = new Date();
+console.log(time);
+
+ 
