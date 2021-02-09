faa694fe38963d2f32e83fc8f9d8f6012096076d
aila marie fruponga <ailamariefruponga@gmail.com>
2021-02-09 16:09:48 +0800

    UC2-TESDA
    ASSIGNMENT

--- LO/LO4/loops.js
+++ LO/LO4/loops.js
@@ -3,14 +3,14 @@
 // for(let i = 0; i < 10; i++) {
 //    if(i === 2) {
 //     console.log('2 is my favorite number');
-    
+//     continue;
 //    }
 //    if(i === 5) {
-//     console.log('Stop the loop');
-//     break;
-//    }
+//      console.log('Stop the loop');
+//      break;
+//     }
 
-//    console.log(`Number ${i}`);
+//     console.log(`Number ${i}`);
 // }
 
 // // While loop
